ed666c8d9fd72bb670a2f86495f3eebfa991f210
mikotorz <95578784+mikotorz@users.noreply.github.com>
2024-11-17 22:15:40 +0900

    Update auto_configs.py

--- auto_configs.py
+++ auto_configs.py
@@ -1,11 +1,11 @@
-HINTED = True  # 是否下载 Hinted 版本
+HINTED = False  # 是否下载 Hinted 版本
 
 REGULAR_SOURCE = 'SarasaUiSC-Regular'  # 标准字体来源的文件名
 BOLD_SOURCE = 'SarasaUiSC-Bold'  # 粗体来源的文件名
 LIGHT_SOURCE = 'SarasaUiSC-Light'  # 细体来源的文件名
 SIMSUN_SOURCE = 'SarasaUiSC-Regular'  # 宋体来源的文件名
 
-COPYRIGHT = 'Made from sarasa by chenh'  # 字体的 Copyright
+COPYRIGHT = 'Made from sarasa by chenh and edited by mikotorz'  # 字体的 Copyright
 
 TEMP_DIR = './temp'  # 临时目录
 RESULT_DIR = './result'  # 结果目录
