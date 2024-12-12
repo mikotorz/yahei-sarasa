8c7ad06e28b81afc03a3220249c58b713d61b21b
mikotorz <95578784+mikotorz@users.noreply.github.com>
2024-12-12 10:06:42 +0900

    edit

--- blex/auto_configs.py
+++ blex/auto_configs.py
@@ -3,7 +3,7 @@ BOLD_SOURCE = 'IBMPlexSansSC-Bold'  # 粗体来源的文件名
 LIGHT_SOURCE = 'IBMPlexSansSC-Light'  # 细体来源的文件名
 SIMSUN_SOURCE = 'IBMPlexSansSC-Text'  # 宋体来源的文件名
 
-COPYRIGHT = 'Made from IBMPlexSansSC mikotorz'  # 字体的 Copyright
+COPYRIGHT = 'Made from IBMPlexSansSC by mikotorz'  # 字体的 Copyright
 
 TEMP_DIR = './temp/unhinted'  # 临时目录
 RESULT_DIR = './result'  # 结果目录
