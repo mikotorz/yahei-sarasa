332b9ccb822ab79c6cdacccfce1c81a7c9dd0234
mikotorz <damenamikoto@gmail.com>
2024-12-14 18:05:20 +0900

    Update font sources and copyright information in auto_configs.py

--- blex/auto_configs.py
+++ blex/auto_configs.py
@@ -1,9 +1,9 @@
-REGULAR_SOURCE = 'IBMPlexSansSC-Text'  # 标准字体来源的文件名
-BOLD_SOURCE = 'IBMPlexSansSC-Bold'  # 粗体来源的文件名
-LIGHT_SOURCE = 'IBMPlexSansSC-Light'  # 细体来源的文件名
-SIMSUN_SOURCE = 'IBMPlexSansSC-Text'  # 宋体来源的文件名
+REGULAR_SOURCE = 'PingFangSC-Regular'  # 标准字体来源的文件名
+BOLD_SOURCE = 'PingFangSC-Bold'  # 粗体来源的文件名
+LIGHT_SOURCE = 'PingFangSC-Light'  # 细体来源的文件名
+SIMSUN_SOURCE = 'PingFangSC-Regular'  # 宋体来源的文件名
 
-COPYRIGHT = 'Made from IBMPlexSansSC by mikotorz'  # 字体的 Copyright
+COPYRIGHT = 'Made from PingFangSC by mikotorz'  # 字体的 Copyright
 
-TEMP_DIR = './temp/unhinted'  # 临时目录
+TEMP_DIR = './temp'  # 临时目录
 RESULT_DIR = './result'  # 结果目录
