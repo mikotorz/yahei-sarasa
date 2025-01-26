5c4b92ddf6eec8318ca1919c7d8ae556e2f44fa9
mikotorz <damenamikoto@gmail.com>
2025-01-27 05:20:19 +0900

    Update font sources and add installation commands in auto_configs.py and yahei_sarasa.ipynb

--- blex/auto_configs.py
+++ blex/auto_configs.py
@@ -1,9 +1,9 @@
-REGULAR_SOURCE = 'PingFangSC-Regular'  # 标准字体来源的文件名
-BOLD_SOURCE = 'PingFangSC-Bold'  # 粗体来源的文件名
-LIGHT_SOURCE = 'PingFangSC-Light'  # 细体来源的文件名
-SIMSUN_SOURCE = 'PingFangSC-Regular'  # 宋体来源的文件名
+REGULAR_SOURCE = 'LXGWNeoXiHeiScreenFull'  # 标准字体来源的文件名
+BOLD_SOURCE = 'SarasaUiSC-Bold'  # 粗体来源的文件名
+LIGHT_SOURCE = 'SarasaUiSC-Light'  # 细体来源的文件名
+SIMSUN_SOURCE = 'LXGWNeoZhiSongScreenFull'  # 宋体来源的文件名
 
-COPYRIGHT = 'Made from PingFangSC by mikotorz'  # 字体的 Copyright
+COPYRIGHT = 'Made from SarasaUiSC, LXGWNeoXiHeiScreenFull & LXGWNeoZhiSongScreenFull by mikotorz'  # 字体的 Copyright
 
 TEMP_DIR = './temp'  # 临时目录
 RESULT_DIR = './result'  # 结果目录
